770e0a339ff77a69e7bc1ee2c054997511937615
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-02-04 01:02:00 -0300

    Atualização

---
+++ Section-4/08-consoleLog/scripts.js
@@ -0,0 +1,7 @@
+let idade = 29;
+let nome = "Gustavo";
+
+console.log(idade);
+console.log(nome);
+
+console.log(`o meu nome e ${nome}, e tenho ${idade} anos`)
---
+++ Section-4/11-elseIf/scripts.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+
+if (a + b == 3) {
+	console.log('O resultado e 3');
+} else if (a == b){
+	console.log('O valor de a e 4');
+} else if (b == 3) {
+	console.log('O valor de b e 3')
+} else {
+	console.log('Nenhuma das condições acima!');
+}
